5d4b3c14dc56b6b10cbbac31e1d9735d4b1757e3
Pachi-Dev <pachi.claros@gmail.com>
2025-07-14 17:22:37 -0600

    feat: agregar la exportación de patrocinadores y corregir formato en constans_logos.js

--- src/data/constans_logos.js
+++ src/data/constans_logos.js
@@ -1,4 +1,5 @@
 
+  
 const exhibitors = [
     { src: '/img/exhibitors/flytek.webp', alt: 'Flytek Innovations', width: 200 },
     { src: '/img/exhibitors/GCE-Aero.webp', alt: 'GCE Aero Drone', width: 170 },
@@ -7,4 +8,16 @@ const exhibitors = [
 ]
 
 
-export { exhibitors,  }
+const sponsors = [
+    { src: '/img/logo_INA-vertical.webp', alt: 'MISUMI', link: 'https://ina.com.mx/', width: 150 },
+    { src: '/img/italianExhibitionGroup.webp', alt: 'Mitsubishi', link: 'https://www.iegexpo.it/en/', width: 180 },
+    { src: '/img/deutscheMesse.webp', alt: 'Schneider', link: 'https://www.messe.de/en/', width: 250 },
+    { src: '/img/airmobility.webp', alt: 'NSK', link: 'https://airmobility.com.mx/', width: 180 },
+    { src: '/img/logo_INA-vertical.webp', alt: 'MISUMI', link: 'https://ina.com.mx/', width: 150 },
+    { src: '/img/italianExhibitionGroup.webp', alt: 'Mitsubishi', link: 'https://www.iegexpo.it/en/', width: 180 },
+    { src: '/img/deutscheMesse.webp', alt: 'Schneider', link: 'https://www.messe.de/en/', width: 250 },
+    { src: '/img/airmobility.webp', alt: 'NSK', link: 'https://airmobility.com.mx/', width: 180 },
+]
+
+
+export { exhibitors, sponsors }
