aac3e77d9ca5ec25adcd4228c12fb5682595338c
Pachi-Dev <pachi.claros@gmail.com>
2025-09-22 08:51:46 -0600

    feat: agregar nuevos logos de expositores "Roca Defense" e "Imago"

--- src/data/constans_logos.js
+++ src/data/constans_logos.js
@@ -4,7 +4,8 @@ const exhibitors = [
     { src: '/img/exhibitors/flytek.webp', alt: 'Flytek Innovations', width: 200 },
     { src: '/img/exhibitors/GCE-Aero.webp', alt: 'GCE Aero Drone', width: 170 },
     { src: '/img/airmobolity_footer.png', alt: 'Air Mobility', width: 250 },
-    
+    { src: '/img/exhibitors/rocadefense.webp', alt: 'Roca Defense', width: 200 },
+    { src: '/img/exhibitors/imago.webp', alt: 'Imago', width: 200 },
 ]
 
 
